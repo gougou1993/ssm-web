be60e8fac88cf86ceb39ac69d415714d2d9f9ae7
JiaXiaohei <i@jiaxiaohei.com>
2017-08-25 23:10:33 +0800

    新增ssm-motan-api 统一存放ssm-motan module/web 接口、实体类、工具类

---
+++ ssm-motan-api/src/main/java/cn/jxh/ssm/common/page/PageProperty.java
@@ -0,0 +1,89 @@
+package cn.jxh.ssm.common.page;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class PageProperty implements Serializable {
+
+    private int startRow;// 查询起点
+    private int pageSize;// 查询数量，0表示全部
+    private String orderColumn;// 排序列
+    private String orderDir;// 排序顺序
+    private HashMap<String, Object> paramMap; // 参数map
+    private int draw;
+
+    public PageProperty() {
+        this.startRow = 0;
+        this.pageSize = 10;
+        this.orderColumn = "";
+        this.orderDir = "";
+        this.draw =0;
+    }
+
+    public int getStartRow() {
+        return startRow;
+    }
+
+    public void setStartRow(int startRow) {
+        this.startRow = startRow;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public String getOrderColumn() {
+        return orderColumn;
+    }
+
+    public void setOrderColumn(String orderColumn) {
+        this.orderColumn = orderColumn;
+    }
+
+    public String getOrderDir() {
+        return orderDir;
+    }
+
+    public void setOrderDir(String orderDir) {
+        this.orderDir = orderDir;
+    }
+
+    public HashMap<String, Object> getParamMap() {
+        initParamMap();
+        return paramMap;
+    }
+
+    public void setParamMap(HashMap<String, Object> paramMap) {
+        this.paramMap = paramMap;
+    }
+
+    public void putParamMap(String name, Object o) { // 增加参数
+        initParamMap();
+        paramMap.put(name, o);
+    }
+
+    public void clearParamMap() { // 增加参数
+        initParamMap();
+        paramMap.clear();
+    }
+
+    public void initParamMap() {// 初始化参数列表
+        if (paramMap == null) {
+            paramMap = new HashMap();
+        }
+    }
+
+    public int getDraw() {
+        return draw;
+    }
+
+    public void setDraw(int draw) {
+        this.draw = draw;
+    }
+}
---
+++ ssm-motan-api/src/main/java/cn/jxh/ssm/entity/SeMenu.java
@@ -0,0 +1,118 @@
+package cn.jxh.ssm.entity;
+
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class SeMenu implements Serializable {
+
+    private Long id;
+
+    private String menucode;
+
+    private String parentcode;
+
+    private String captionname;
+
+    private Long menuorder;
+
+    private String sourceurl;
+
+    private String classname;
+
+    private Integer visible;
+
+    private Date entrytime;
+
+    private Date updatetime;
+
+    private Long updateid;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMenucode() {
+        return menucode;
+    }
+
+    public void setMenucode(String menucode) {
+        this.menucode = menucode;
+    }
+
+    public String getParentcode() {
+        return parentcode;
+    }
+
+    public void setParentcode(String parentcode) {
+        this.parentcode = parentcode;
+    }
+
+    public String getCaptionname() {
+        return captionname;
+    }
+
+    public void setCaptionname(String captionname) {
+        this.captionname = captionname;
+    }
+
+    public Long getMenuorder() {
+        return menuorder;
+    }
+
+    public void setMenuorder(Long menuorder) {
+        this.menuorder = menuorder;
+    }
+
+    public String getSourceurl() {
+        return sourceurl;
+    }
+
+    public void setSourceurl(String sourceurl) {
+        this.sourceurl = sourceurl;
+    }
+
+    public String getClassname() {
+        return classname;
+    }
+
+    public void setClassname(String classname) {
+        this.classname = classname;
+    }
+
+    public Integer getVisible() {
+        return visible;
+    }
+
+    public void setVisible(Integer visible) {
+        this.visible = visible;
+    }
+
+    public Date getEntrytime() {
+        return entrytime;
+    }
+
+    public void setEntrytime(Date entrytime) {
+        this.entrytime = entrytime;
+    }
+
+    public Date getUpdatetime() {
+        return updatetime;
+    }
+
+    public void setUpdatetime(Date updatetime) {
+        this.updatetime = updatetime;
+    }
+
+    public Long getUpdateid() {
+        return updateid;
+    }
+
+    public void setUpdateid(Long updateid) {
+        this.updateid = updateid;
+    }
+}
